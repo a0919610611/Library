118c9684425036fa469ed9a1d8b87a10c6e1dd36
傅裕夫 <a0919610611@gmail.com>
2016-11-27 23:12:02 +0800

    update README

--- Library/settings.py
+++ Library/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_docs',
+    'api',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -70,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Library.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
@@ -80,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -100,13 +99,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = '/admin/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+FILE_CHARSET = 'utf-8'
+DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Library
-Library borrow system for Software Development Pratice
+BackEnd for library borrow system for Software Development Pratice
+
+[FrontEnd](https://github.com/calee0219/SDP-Library-System) Build By calee0219  
+
+#Technology Stack
+Python3   
+ [Django](https://www.djangoproject.com/
+)   
+[Django Rest Framework] (http://www.django-rest-framework.org/)  
+[Django Rest Framework Docs] (https://github.com/manosim/django-rest-framework-docs)
+#Database
+Sqlite in Development  
+MariaDB in Production
