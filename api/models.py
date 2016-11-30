2613d5b9a2355747c8743fdec5d98df5c0a575a7
傅裕夫 <a0919610611@gmail.com>
2016-12-01 01:01:26 +0800

    reconstruct delete account all transfer to api for restful

--- api/models.py
+++ api/models.py
@@ -1,27 +1,84 @@
+from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth import get_user_model
-from account.models import CustomUser
+from datetime import datetime
 
 
-# User = get_user_model()
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('Email is required')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if not email:
+            raise ValueError('Email is required')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 # Create your models here.
-# class Author(models.Model):
-#     name = models.CharField('name', max_length=100)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField('Username', max_length=30, unique=True)
+    student_id = models.CharField('StudentId', max_length=10, blank=True, null=True)
+    address = models.CharField('Address', max_length=255, blank=True, null=True)
+    email = models.EmailField('Email', unique=True)
+    birthday = models.CharField('BirthDay', default="", max_length=20)
+    first_name = models.CharField('First name', max_length=50, default="")
+    last_name = models.CharField('Last name', max_length=50, default="")
+    phone_number = models.CharField('Phone Number', max_length=50, blank=True, null=True)
+    is_staff = models.BooleanField('staff status', default=False)
+    is_active = models.BooleanField('active status', default=True)
+    date_joined = models.DateTimeField('join date', auto_now=datetime.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', ]
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_full_name(self):
+        return self.first_name + self.last_name
+
+
+class BarCode(models.Model):
+    bar_code = models.CharField('Bar Code', max_length=100)
+    book = models.ForeignKey('Book')
+    is_borrowed = models.BooleanField('borrowed', default=False)
 
 
 class Book(models.Model):
     title = models.CharField('title', max_length=100)
     author = models.CharField('Author', max_length=100)
+    publisher = models.CharField('Publisher', max_length=100)
+    call_number = models.CharField('Call Number', max_length=100)
     ISBN = models.CharField('ISBN', max_length=100)
-    total_number = models.IntegerField('Total Number')
-    borrowed_number = models.IntegerField('Borrowed Number', default=0)
-    users = models.ManyToManyField(CustomUser)
 
 
 class BorrowInformation(models.Model):
-    book = models.ForeignKey('Book')
+    barcode = models.ForeignKey('BarCode')
     user = models.ForeignKey(CustomUser)
     borrowed_time = models.DateTimeField()
     due_time = models.DateTimeField()
